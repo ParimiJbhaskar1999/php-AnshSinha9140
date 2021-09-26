05ab1f9de15ab14cefbe00b21710448e84c46c7a
AnshSinha9140 <anshsinha8604@gmail.com>
2021-09-26 18:51:08 +0530

    changes to frontend

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-# php-starter
+#ComicTube
 
-If you are reading this in your assignment repo, please read [assignment submission guidelines](https://learn.rtcamp.com/campus/php-assignments/guidelines/) before proceeding.
-
-Then make sure to replace content of the file with information relevant to your assignment. 
-
-Writing your own README.md file is required anyway as per [assignment submission guidelines](https://learn.rtcamp.com/campus/php-assignments/guidelines/).
+![License](https://img.shields.io/badge/HTML-website-sky)
+![License](https://img.shields.io/badge/Php-Backend-blue)
---
+++ pages/login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" , href="./style.css" />
+    <title>ComicTube</title>
+  </head>
+  <body>
+    <section class="header">
+      <div id="head">
+        <h3>ComicTube</h3>
+      </div>
+    </section>
+
+    <section id="registration">
+      <div id="register-card">
+        <div id="register-header">
+          <h3>Login</h3>
+        </div>
+        <div class="form-div">
+          <form>
+            <input
+              class="field"
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
+            <input
+              class="field"
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+
+            <button type="submit" class="btn">Submit</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
